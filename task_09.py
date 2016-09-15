d5ebe242bc26a3cf5b3bc5e14574e4db885bf08c
Iuliia Ambrokh <iuliia.ambrokh@spsmail.cuny.edu>
2016-09-15 04:44:56 +0000

    I added docstring.

--- task_09.py
+++ task_09.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.
+   This is a multi-line docstring and this docstring is going to be right!
+   It seems like it works!"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
 INFINITE_IMPROBABILITY = 'browning motion'
+
+import task_09
+
+help (task_09)
