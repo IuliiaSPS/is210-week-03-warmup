375c5c3b6352df04c2358f1ad91439e88896050c
Iuliia Ambrokh <iuliia.ambrokh@spsmail.cuny.edu>
2016-09-16 15:43:57 +0000

    Task_09

--- task_09.py
+++ task_09.py
@@ -4,10 +4,10 @@
    This is a multi-line docstring and this docstring is going to be right!
    It seems like it works!"""
 
+import task_09
+
 THE_ANSWER_TO_EVERYTHING = 42
 
 INFINITE_IMPROBABILITY = 'browning motion'
 
-import task_09
-
-help (task_09)
+help(task_09)
